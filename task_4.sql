b752f99d09db1e4964449144d02ad5032718f52d
brayan otieno <briandan2058@gmail.com>
2025-01-31 14:34:15 +0100

    Add a updated script

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Use the provided database (this is an argument passed to the mysql command)
+USE alx_book_store;
+
+-- Query the INFORMATION_SCHEMA to get the full description of the 'Books' table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
